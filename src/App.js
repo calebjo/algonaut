9048f0e239ed70194b1f2aac7afb93c3923a387f
calebjo <caleb.ri.jones@gmail.com>
2022-03-21 20:22:49 -0400

    Add initial component structure for top nav

--- src/App.js
+++ src/App.js
@@ -6,6 +6,36 @@ function App() {
     <div className="app">
         <div className="content">
             <div className="top-nav">
+                <div className="nav-left">
+                    <div className="nav-logo">
+
+                    </div>
+                </div>
+                <div className="nav-center">
+
+                </div>
+                <div className="nav-right">
+                    <div className="nav-socials">
+                        <div className="social-link">
+                            <a href="https://github.com/calebjo/">
+                                GITHUB
+                            </a>
+                        </div>
+                        <div className="social-link">
+                            <a href="https://www.linkedin.com/in/caleb-jones-928521126/">
+                                LINKEDIN
+                            </a>
+                        </div>
+                        <div className="social-link">
+                            <a href="https://calebjones.io/">
+                                WEBSITE
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="content-body">
+
             </div>
         </div>
     </div>
